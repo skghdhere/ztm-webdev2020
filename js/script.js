d8188c1120fb12f1d1d03e351fdb2f99ffa994de
SKGHD <sangramgorai@gmail.com>
2020-11-10 17:20:07 +0530

    fixed redirect home

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector(".nav");
 
 nav.addEventListener("click", () => {
-  window.location = "/index.html";
+  window.location.href = "https://skghdhere.github.io/ztm-webdev2020/";
 });
