76cbf6b75e5f2f13c839d4ef6772e4fbf80dfcdb
SKGHD <sangramgorai@gmail.com>
2020-11-11 21:17:31 +0530

    last commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@ const nav = document.querySelector(".nav");
 nav.addEventListener("click", () => {
   window.location.href = "https://skghdhere.github.io/ztm-webdev2020/";
 });
+
+// Go to top button
+var btn = document.querySelector("#button");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 250 ||
+    document.documentElement.scrollTop > 250
+  ) {
+    btn.className = "show";
+  } else btn.className = "";
+};
+
+btn.addEventListener("click", () => scrollTo(0, 0));
